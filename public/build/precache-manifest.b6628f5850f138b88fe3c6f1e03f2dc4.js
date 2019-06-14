a135dc787b02840b8fabd1b82c05f4e3b23fdd85
Raymond Akalonu <raymondosy1@gmail.com>
2019-06-14 16:26:31 +0100

    update build

---
+++ public/build/precache-manifest.b6628f5850f138b88fe3c6f1e03f2dc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f27c39f83ce25b3c55404372ac03daf2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa2edf6adb274aaa1762",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "9d3c3541591acc6ccb93",
+    "url": "/static/js/2.accbb427.chunk.js"
+  },
+  {
+    "revision": "fa2edf6adb274aaa1762",
+    "url": "/static/js/main.cd1d008d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7dab9996f15513443ab7ec4d1c04cab9",
+    "url": "/static/media/logo.7dab9996.jpg"
+  }
+]);
